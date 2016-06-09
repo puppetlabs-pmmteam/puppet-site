6fbcb85bdd5f2bdb43529a1f8b78305e353d88a5
Carl Caum <carl@carlcaum.com>
2016-06-09 14:47:21 -0700

    Add jenkins job

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -4,10 +4,11 @@ class profile::jenkins {
     configure_firewall => true,
   }
 
-  #jenkins::job { "control-repo":
-  #  ensure  => present,
-  #  enabled => true,
-  #}
+  jenkins::job { "control-repo":
+    ensure  => present,
+    enabled => true,
+    config  => epp('profile/jenkins_job_config.epp'),
+  }
 
   jenkins::plugin { 'workflow-puppet-enterprise':
     source => 'http://int-resources.ops.puppetlabs.net/carl/workflow-puppet-enterprise.hpi',
