f71c9a5404cc5327eb6b4ad5fb570ef596a7cf54
Carl Caum <carl@carlcaum.com>
2016-06-09 13:54:30 -0700

    Do not manage control repo job yet

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -4,10 +4,10 @@ class profile::jenkins {
     configure_firewall => true,
   }
 
-  jenkins::job { "control-repo":
-    ensure  => present,
-    enabled => true,
-  }
+  #jenkins::job { "control-repo":
+  #  ensure  => present,
+  #  enabled => true,
+  #}
 
   jenkins::plugin { 'workflow-puppet-enterprise':
     source => 'http://int-resources.ops.puppetlabs.net/carl/workflow-puppet-enterprise.hpi',
