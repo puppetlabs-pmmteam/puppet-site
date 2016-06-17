34023955dcb3d85cf1b4aadbf70951ff4ffcc1e9
Carl Caum <carl@carlcaum.com>
2016-06-17 15:51:23 -0700

    Generate component IDs globally, not per host

--- site/profile/lib/puppet/functions/make_component_titles.rb
+++ site/profile/lib/puppet/functions/make_component_titles.rb
@@ -3,13 +3,16 @@
 # @return [Hash] Application instance parameters
 Puppet::Functions.create_function(:make_component_titles) do
   def make_component_titles(parameters, name_prefix)
+    global_component_count = 0
     return_hash = parameters
 
     parameters['nodes'].each do |node, components|
-      components.each_with_index do |component, index|
+      components.each do |component|
         unless component.match /^[\w:]+\[.*\]$/
-          return_hash['nodes'][node][index] = "#{component}[#{name_prefix}-#{index}]"
+          return_hash['nodes'][node][global_component_count] = "#{component}[#{name_prefix}-#{global_component_count}]"
         end
+
+        global_component_count += 1
       end
     end
 
