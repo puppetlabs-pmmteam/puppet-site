215634bcb1004addeeeebebc5ffab7390c2b1f42
Carl Caum <carl@carlcaum.com>
2016-06-17 17:36:26 -0700

    Better parse applications.yaml

--- site/profile/lib/puppet/functions/make_application_parameters.rb
+++ site/profile/lib/puppet/functions/make_application_parameters.rb
@@ -3,17 +3,19 @@
 # @return [Hash] Application instance parameters
 Puppet::Functions.create_function(:make_application_parameters) do
   def make_application_parameters(parameters)
+    parameters['parameters'] = Hash.new unless parameters.has_key?('parameters')
     components = parameters['components']
     nodes_hash = Hash.new
 
     components.each do |component, nodes|
-      [component].each do |comp|
-        nodes.each do |node|
-          unless nodes_hash.hash_key? node
-            nodes_hash[node] = Array.new
+      [*component].each do |comp|
+        nodes.each do |node_name|
+          node_resource_format = "Node[#{node_name}]"
+          unless nodes_hash.has_key? node_resource_format
+            nodes_hash[node_resource_format] = Array.new
           end
 
-          nodes_hash[node] << "#{comp}[#{node}]"
+          nodes_hash[node_resource_format] << "#{comp}[#{node_name}]"
         end
       end
     end
