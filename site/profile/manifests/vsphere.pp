381498b8271f572195e86440b5c121d140e6a4b4
Carl Caum <carl@carlcaum.com>
2016-08-15 09:56:58 -0700

    Support vsphere config profile

---
+++ site/profile/manifests/vsphere.pp
@@ -0,0 +1,14 @@
+class profile::vsphere(
+  $host,
+  $username,
+  $password,
+) {
+
+  file { '/etc/puppetlabs/puppet/vcenter.conf':
+    content => epp('profile/vcenter.conf.epp', {'username' => $username, 'password' => $password, 'host' => $host }),
+    owner   => 'pe-puppet',
+    group   => 'pe-puppet',
+    mode    => 0644,
+  }
+
+}
