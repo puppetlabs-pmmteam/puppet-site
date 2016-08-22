d8048bd40db3c1608681ccb798c93fbd45bd5038
Carl Caum <carl@carlcaum.com>
2016-08-22 14:51:37 -0700

    Do not purge unamanged firewall rules on Docker hosts

--- site/profile/manifests/firewall.pp
+++ site/profile/manifests/firewall.pp
@@ -2,10 +2,13 @@ class profile::firewall {
   case $::osfamily {
     default: {
 
-      resources { 'firewall':
-        purge => true,
+      #Docker manages its own firewall rules so we shouldn't purge ones
+      #we don't recognize
+      if ! $networking['interfaces']['docker0'] {
+        resources { 'firewall':
+          purge => true,
+        }
       }
-
     }
     'windows', 'Solaris': {
       warning("osfamily ${::osfamily} not supported by profile::firewall")
