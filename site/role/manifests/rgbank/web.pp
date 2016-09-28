ee3bbcacc3bcb229207acea268f6a2b806ea2a55
Carl Caum <carl@carlcaum.com>
2016-09-28 15:53:46 -0700

    Manage a role and profile for RG Bank web component
    This commit turns the role::rgbank::web class into a proper role and
    uses the rgbank::web::docker::image class to manage the bits.

--- site/role/manifests/rgbank/web.pp
+++ site/role/manifests/rgbank/web.pp
@@ -1,58 +1,6 @@
 class role::rgbank::web {
-  include 'dummy_service'
-  include apache
-
-  if $install_dir {
-    $install_dir_real = $install_dir
-  } else {
-    $install_dir_real = "/opt/rgbank-${name}"
-  }
-
-  archive { "rgbank-build-${version}":
-    ensure     => present,
-    url        => $source,
-    target     => "${install_dir_real}/wp-content/themes/rgbank",
-    checksum   => false,
-    src_target => '/tmp'
-  }
-
-  wordpress::instance::app { "rgbank_${name}":
-    install_dir          => $install_dir_real,
-    install_url          => 'http://wordpress.org',
-    version              => '4.5.2',
-    db_host              => $db_host,
-    db_name              => $db_name,
-    db_user              => $db_user,
-    db_password          => $db_password,
-    wp_owner             => 'root',
-    wp_group             => '0',
-    wp_lang              => '',
-    wp_config_content    => undef,
-    wp_plugin_dir        => 'DEFAULT',
-    wp_additional_config => 'DEFAULT',
-    wp_table_prefix      => 'wp_',
-    wp_proxy_host        => '',
-    wp_proxy_port        => '',
-    wp_multisite         => false,
-    wp_site_domain       => '',
-    wp_debug             => false,
-    wp_debug_log         => false,
-    wp_debug_display     => false,
-    notify               => Service['httpd'],
-  }
-
-  file { "${install_dir_real}/wp-content/uploads":
-    ensure  => directory,
-    owner   => apache,
-    group   => apache,
-    recurse => true,
-    require => Wordpress::Instance::App["rgbank_${name}"],
-  }
-
-  apache::listen { $listen_port: }
-
-  apache::vhost { $::fqdn:
-    docroot => $install_dir_real,
-    port    => $listen_port,
-  }
+  include profile::apache
+  include profile::docker
+  include profile::mysql::client
+  include profile::rgbank::web
 }
