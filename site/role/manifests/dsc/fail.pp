c8ce65590fcd510eaf628838b74b3872d89334fb
Iristyle <Iristyle@github>
2016-10-07 19:03:13 -0700

    PuppetConf 2016 DSC Demos

--- site/role/manifests/dsc/fail.pp
+++ site/role/manifests/dsc/fail.pp
@@ -1,4 +1,19 @@
 class role::dsc::fail {
-  
+
   include profile::windows::baseline
+
+  # runtime error during application of resource
+  dsc_file {'bad_test_dir':
+    dsc_ensure          => 'present',
+    dsc_type            => 'Directory',
+    dsc_destinationpath => 'Q:/not/here'
+  }
+
+  # Puppet parameter validation won't run your manifest
+  # dsc_file {'puppet_validation':
+  #  dsc_ensure          => 'present',
+  #  dsc_type            => 'Thing',
+  #  dsc_destinationpath => 'C:\\'
+  # }
+
 }
