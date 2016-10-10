7a6f7f427b01e061af939b55422d68948eed79a8
Spencer <spencer.seebald@puppetlabs.com>
2016-10-10 12:56:22 -0700

    Update pending.rb

--- site/profile/lib/facter/pending.rb
+++ site/profile/lib/facter/pending.rb
@@ -3,6 +3,6 @@
 Facter.add('windows_updates') do
   confine :kernel => 'windows'
   setcode do
-    Facter::Core::Execution.exec('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noninteractive -noprofile -command "import-module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate\Get-PendingUpdate.ps1; Get-PendingUpdate -ComputerName puppetconf | select-object Title,MsrcSeverity"')
+    Facter::Core::Execution.exec('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noninteractive -noprofile -command "import-module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate\Get-PendingUpdate.ps1; Get-PendingUpdate | select-object Title,MsrcSeverity"')
   end
 end
