08697e37bf86b4f71ec57a1a2cea9d124b771dde
Spencer <spencer.seebald@puppetlabs.com>
2016-10-10 13:24:52 -0700

    Update baseline.pp

--- site/profile/manifests/windows/baseline.pp
+++ site/profile/manifests/windows/baseline.pp
@@ -1,5 +1,11 @@
 class profile::baseline {
 
+  exec { 'rename-admin':
+    command   => '$(Get-WMIObject Win32_UserAccount -Filter "Name=\'Administrator\'").Rename("PuppetAdmin")',
+    unless    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'Administrator\'") { exit 1 }',
+    provider  => powershell,
+  }
+
   class { 'chocolatey': }
 
   package { 'powershell':
