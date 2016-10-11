073878cfabbb7992f80cbb45031e56cd2c38533a
Carl Caum <carl@carlcaum.com>
2016-10-11 16:27:11 -0700

    Use proper secrets profile parameter

--- site/role/manifests/demo/secrets.pp
+++ site/role/manifests/demo/secrets.pp
@@ -2,8 +2,7 @@ class role::demo::secrets {
   include ::profile::common
   include ::profile::firewall
   
-  
   class { '::profile::secret_example':
-    db_password => Sensitive(hiera('secret_content')),
+    secret_content => Sensitive(hiera('secret_content')),
   }
 }
